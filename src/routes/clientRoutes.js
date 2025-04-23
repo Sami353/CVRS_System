f669e12be9744e2609dca8e2d311a928219e7d44
Sami353 <malisami878@gmail.com>
2025-04-23 14:32:48 +0545

    chore: rebase develop branch to feature branch

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Dashboard = React.lazy(() => import('../views/client/dashboard/Dashboard'))
-const childRegister = React.lazy(() => import('../views/client/childRegister/childRegister'))
-const listHospital = React.lazy(() => import('../views/client/listHospital/listHospital'))
-const addHospital = React.lazy(() => import('../views/client/addHospital/addHospital'))
-const addVaccine = React.lazy(()=> import('../views/client/addVaccine/addVaccine'))
-const listVaccine = React.lazy(()=> import('../views/client/listVaccine/listVaccine'))
-const listChild = React.lazy(() => import('../views/client/listChild/listChild'))
+// const Dashboard = React.lazy(() => import('../views/client/dashboard/Dashboard'))
+// const childRegister = React.lazy(() => import('../views/client/childRegister/childRegister'))
+// const listHospital = React.lazy(() => import('../views/client/listHospital/listHospital'))
+// const addHospital = React.lazy(() => import('../views/client/addHospital/addHospital'))
+// const addVaccine = React.lazy(()=> import('../views/client/addVaccine/addVaccine'))
+// const listVaccine = React.lazy(()=> import('../views/client/listVaccine/listVaccine'))
+// const listChild = React.lazy(() => import('../views/client/listChild/listChild'))
+
+const Dashboard = React.lazy(() => import('../views/client/dashboard/Dashboard.js'))
+const childRegister = React.lazy(() => import('../views/client/childRegister/childRegister.js'))
+const listHospital = React.lazy(() => import('../views/client/listHospital/listHospital.js'))
+const addHospital = React.lazy(() => import('../views/client/addHospital/addHospital.js'))
+const addVaccine = React.lazy(()=> import('../views/client/addVaccine/addVaccine.js'))
+const listVaccine = React.lazy(()=> import('../views/client/listVaccine/listVaccine.js'))
+const listChild = React.lazy(() => import('../views/client/listChild/listChild.js'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
